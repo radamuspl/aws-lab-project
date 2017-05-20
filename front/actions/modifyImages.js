947c2a2ca48a5667d41a12929591a9f32452f02e
Piotr Krawiranda <piotrkrawiranda@gmail.com>
2017-05-20 18:05:58 +0200

    [Front] sqs added

--- front/actions/modifyImages.js
+++ front/actions/modifyImages.js
@@ -2,18 +2,50 @@
  * Created by piotr on 17.05.17.
  */
 
+"use strict";
 
+var AWS = require("aws-sdk");
+var _ = require("lodash");
+
+AWS.config.loadFromPath('config/aws-config.json');
+var sqs = new AWS.SQS();
 
 var task =  function(request, callback){
 
-    if(!request.body.keys) {
-        callback("Any images selected")
-    }
+    if(!request.body.keys) callback("ERROR: Any images selected");
+    if(request.body.keys.length > 10) callback("ERROR: more than 10 items selected");
 
-    // TODO add SQS
+    var entries = _.map( request.body.keys, function (key, index) {
+        return {
+            Id: index.toString(),
+            MessageBody: JSON.stringify(
+                {
+                    option: request.body.option,
+                    key: key
+                }
+            )
+        }
+    });
 
-    callback(null, request.body);
+    var params = {
+        QueueUrl: 'https://sqs.us-west-2.amazonaws.com/894955361035/AWS-project',
+        Entries: entries
+    };
 
+    sqs.sendMessageBatch(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            callback(err);
+        }
+        else {
+            console.log(data);
+            if(!data.Failed) {
+                callback(null, "all msg upload");
+            } else {
+                callback(null, "sorry " + data.Failed.length + " msg failed")
+            }
+        }
+    });
 };
 
 exports.action = task;
