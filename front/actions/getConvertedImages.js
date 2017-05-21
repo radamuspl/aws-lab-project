1e5f06f207d741bffd240cfb1f6c0df6d82c10f6
Piotr Krawiranda <piotrkrawiranda@gmail.com>
2017-05-21 23:12:35 +0200

    [Front] getConvertedImages added

---
+++ front/actions/getConvertedImages.js
@@ -0,0 +1,41 @@
+/**
+ * Created by piotr on 21.05.17.
+ */
+
+var AWS = require("aws-sdk");
+var _ = require("lodash");
+
+AWS.config.loadFromPath('config/aws-config.json');
+var s3 = new AWS.S3();
+
+var task =  function(request, callback){
+
+    //TODO get bucket from config
+
+    var params = {
+        Bucket: 'aws-lab-project1',
+        Marker: 'convertedImages/',
+        MaxKeys: 50,
+        Prefix: 'convertedImages/'
+    };
+
+    s3.listObjects(params, function(err, data) {
+        if (err) {
+            console.log(err, err.stack);
+            callback(err);
+        }
+        else {
+            var imagesKeys = _.map(data.Contents, 'Key');
+            var images = _.map(imagesKeys, function (key) {
+                return {
+                    key: key,
+                    link: "https://s3-us-west-2.amazonaws.com/aws-lab-project1/" + key
+                }
+            });
+            callback(null, {template: "convertedImages.ejs", params:{images: images}});
+        }
+    });
+
+};
+
+exports.action = task;
